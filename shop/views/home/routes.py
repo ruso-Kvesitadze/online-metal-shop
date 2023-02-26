1a614f7a4af8be59ac06c8f5db7e303a13f3ee37
Kristine Dzneladze <qristina.qristina20010203@gmail.com>
2023-02-27 01:24:45 +0400

    ughhh

--- shop/views/home/routes.py
+++ shop/views/home/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
 
-home_blueprint = Blueprint("main", __name__, template_folder="templates")
+home_blueprint = Blueprint("home", __name__, template_folder="templates")
 
-@home_blueprint.route("/")
+@home_blueprint.route("/home")
 def home():
     return render_template("home/home.html")
 

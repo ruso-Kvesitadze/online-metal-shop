ba9ecfd8f1573916196bfd97528f0455e3092236
Kristine Dzneladze <qristina.qristina20010203@gmail.com>
2023-02-27 23:21:20 +0400

    idk what im doing

--- shop/data.py
+++ shop/data.py
@@ -1,162 +1,162 @@
 products = [
     {
-        "type":"shirt", 
+        "type":"Shirt", 
         "name": "Rammstein",
         "quantity": 5,
         "price": 45,
-        "img-link": " "
+        "img-link": " https://cdn.shopify.com/s/files/1/0272/8991/1349/products/1305034065_600x@2x.jpg?v=1655295709"
     },
     {
-        "type":"shirt", 
+        "type":"Shirt", 
         "name": "Metallica",
         "quantity": 5,
         "price": 45,
-        "img-link": " "
+        "img-link": " https://www.metallica.com/dw/image/v2/BCPJ_PRD/on/demandware.static/-/Sites-met-master/default/dw21408e25/images/hi-res/Master_Of_Puppets_T-Shirt.jpg?sw=650"
 
     },
     {
-        "type":"shirt", 
+        "type":"Shirt", 
         "name": "Iron Maiden",
         "quantity": 5,
         "price": 45,
-        "img-link": " "
+        "img-link": "https://m.media-amazon.com/images/I/A13usaonutL._AC_CLa%7C2140%2C2000%7CA1R%2BESWa2jL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UF1000,1000_QL80_.png "
 
     },
     {
-        "type":"shirt", 
+        "type":"Shirt", 
         "name": "Pink Floyd",
         "quantity": 5,
         "price": 45,
-        "img-link": " "
+        "img-link": " https://m.media-amazon.com/images/I/A13usaonutL._AC_CLa%7C2140%2C2000%7C71eHiUOqwaL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UF1000,1000_QL80_.png "
 
     },
 
     {
-        "type":"shirt",
+        "type":"Shirt",
         "name": "Led Zeppelin",
         "quantity": 5,
         "price": 45,
-        "img-link": " "
+        "img-link": " https://cdn.shopify.com/s/files/1/0948/5022/products/amplified_ledzeppelin_ledzeppelinustour1975_1613127319amplified_ledzeppelin_ledzeppelinustour1975_1548780674ZAV210LUS_CC.jpg_1_1024x1024.jpg?v=1629562326 "
 
     },
 
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Led Zeppelin",
         "quantity": 5,
         "price": 15,
-        "img-link": " "
+        "img-link": " https://cosmo.com.ge/wp-content/uploads/2022/07/kulo-18-min.jpg "
 
     },
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Gojira",
         "quantity": 5,
         "price": 20,
-        "img-link": " ",
+        "img-link": " https://cdn.shopify.com/s/files/1/1405/2230/products/gojirafortitude1_1024x1024.jpg?v=1664597690 ",
 
     },
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Metallica",
         "quantity": 5,
         "price": 10,
-        "img-link": " "
+        "img-link": " https://images-na.ssl-images-amazon.com/images/I/61B6UDYMdWL._SS400_.jpg "
 
     },
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Rammstein",
         "quantity": 5,
         "price": 10,
-        "img-link": " "
+        "img-link": " https://images-na.ssl-images-amazon.com/images/I/51hu11vyJGL.jpg "
 
     },
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Iron Maiden",
         "quantity": 5,
         "price": 10,
-        "img-link": " "
+        "img-link": " https://cdn11.bigcommerce.com/s-zehf9yvjok/images/stencil/600x600/products/3357/6814/O1CN012KoDsoScHSYbZmG__0-item_pic__21059.1662722757.jpg?c=1 "
 
     },
     {
-        "type":"necklace",
+        "type":"Necklace",
         "name": "Pink Floyd",
         "quantity": 5,
         "price": 10,
-        "img-link": " "
+        "img-link": "https://www.woodstocktradeco.com/mm5/graphics/00000002/DH412953604PinkFloydDarkSideNecklace_540x540.jpg "
 
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Rammstein",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": "https://www.zepplingiyim.com/66544-thickbox_default/rammstein-sehnsucht-kapsonlu-sweatshirt.jpg"
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Metallica",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": "https://www.metallica.com/dw/image/v2/BCPJ_PRD/on/demandware.static/-/Sites-met-master/default/dw9b3f8d40/images/hi-res/03-Met_HarvesterOfSorrowTyeDyeHoodie.jpg?sw=650 "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Iron Maiden",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://static.ticimax.cloud/13027/uploads/urunresimleri/buyuk/iron-maiden-kapsonlu-sweatshirt-hoodie-k-6b92.jpg "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Pink Floyd",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://www.sportsdirect.com/images/imgzoom/53/53911103_xxl.jpg "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Gojira",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://rsz.tzy.li/fb/600/600/ffffff/tzy/previews/images/002/313/687/270/original/gojira-official-hoodie-sweatshirt.jpg?1654487981"
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Rotting Christ",
         "quantity": 10,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://tshirtkingship.com/wp-content/uploads/2021/03/Roting-Cross-Hoodie.jpg "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Manowar",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://res.cloudinary.com/teepublic/image/private/s--h0GDIxv8--/t_Resized%20Artwork/c_crop,x_10,y_10/c_fit,h_620/c_crop,g_north_west,h_620,w_465,x_-6,y_0/g_north_west,u_upload:v1446840652:production:blanks:ymwlojdlb9pdlxgcmck4,x_-397,y_-276/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1658307663/production/designs/33419523_0.jpg "
     },
     {       
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Sabaton",
         "quantity": 12,
         "price": 70,
-        "img-link": " "
+        "img-link": " http://www.tisortfabrikasi.com/Uploads/UrunResimleri/buyuk/Sabaton-Kapsonlu-Sweatshirt-Hoodie-Kapus-4167.jpg "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Megadeth",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " http://images.backstreetmerch.com/images/products/bands/clothing/mgdth/bsi_mgdth20.jpg "
     },
     {
-        "type":"hoodie",
+        "type":"Hoodie",
         "name": "Black Sabbath",
         "quantity": 5,
         "price": 70,
-        "img-link": " "
+        "img-link": " https://m.media-amazon.com/images/I/B1i3u9-Q-KS._AC_CLa%7C2140%2C2000%7CB1mEDhBtIXL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_UF894,1000_QL80_.png "
     },
 ]
 
--- shop/views/home/routes.py
+++ shop/views/home/routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
+from shop.models import Product
+home_blueprint = Blueprint("main", __name__, template_folder="templates")
 
-home_blueprint = Blueprint("home", __name__, template_folder="templates")
-
-@home_blueprint.route("/home")
+@home_blueprint.route("/")
 def home():
-    return render_template("home/home.html")
+    product  = Product.query.all()
+    print(f"{product} hi")
+    return render_template("home/home.html", products = product)
 
 # @main_blueprint.route("/admin")
 # def admin_pannel():
